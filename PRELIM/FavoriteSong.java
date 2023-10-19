265670a19f7b435425c81e805236661cab57e393
unknown <OO323@ccs.local>
2023-10-19 18:19:17 +0100

    Updating PRELIM folder

---
+++ PRELIM/FavoriteSong.java
@@ -0,0 +1,14 @@
+public class FavoriteSong {
+   public static void main (String[] args) {
+      
+      System.out.println("Black Magic by Little Mix");
+   
+      System.out.println("\nTake a sip of my secret potion");
+      System.out.println("I'll make you fall in love");
+      System.out.println("For a spell that can't be broken");
+      System.out.println("One drop should be enough");
+      System.out.println("Boy, you belong to me");
+      System.out.println("I got the recipe and it's called Black Magic");
+      
+   }
+}
